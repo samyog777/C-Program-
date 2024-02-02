5453ded85903c0eadc3caa257161e4544cb9e84f
samyog777 <samyogkoirala18@gmail.com>
2024-02-02 20:20:27 +0545

    practice for lab exam

--- lab_report pract/lab6_qn1.c
+++ lab_report pract/lab6_qn1.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 
-struct Employee {
+struct Student {
     char name[50];
     int age;
     int salary;
 };
 
 int main() {
-    struct Employee emp[3];
-    for (int i=0; i<3;i++) {
-        printf("Enter detail of employee %d\n",i+1);
-        printf("Name: ");
-        scanf("%s",emp[i].name);
-        printf("Age: ");
-        scanf("%d",&emp[i].age);
-        printf("Salary: ");
-        scanf("%d",&emp[i].salary);
-    }
-    FILE* file = fopen("/home/samyogkoirala/Documents/Course/Third semester/C/C-program git/lab_report pract/try.txt","w");
+    struct Student s[3];
 
-    if(file == NULL) {
-        printf("Unable to open file. \n");
+    printf("Enter the information of your student: \n");
+    for (int i=0;i<3;i++) {
+        printf("Name: ");
+        scanf("%s",s[i].name);
+        printf("Age: ");
+        scanf("%d",&s[i].age);
+        printf("Salary: ");
+        scanf("%d",&s[i].salary);
+        printf("\n");
+    }
+    
+    FILE* file= fopen("/home/samyogkoirala/Documents/Course/Third semester/C/C-program git/lab_report pract/try.txt","w");
+
+    if (file == NULL) {
+        printf("Error while loading data!");
         return 1;
     }
 
     for (int i=0;i<3;i++) {
-        fprintf(file,"Employee %d\n",i+1);
-        fprintf(file,"Name: %s \n",emp[i].name);
-        fprintf(file,"Age: %d \n",emp[i].age);
-        fprintf(file,"Salary: %d \n",emp[i].salary);
-        fprintf(file,"/n");
+        fprintf(file,"Name: %s\n",s[i].name);
+        fprintf(file,"Age: %d\n",s[i].age);
+        fprintf(file,"Salary: %d\n",s[i].salary);
+        fprintf(file,"\n"); 
     }
+    
     fclose(file);
-    printf("Employee information has been written to d drive");
     return 0;
 }
